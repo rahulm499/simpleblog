807c2fc9bb3476ec27ee6a5b2734124d2dd22ed6
rahulm499 <43981460+rahulm499@users.noreply.github.com>
2021-07-23 21:54:52 +0530

    Delete mongoose.js

--- mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://blogsite:blogsite@cluster0.kjdrn.mongodb.net/blog?retryWrites=true&w=majority', 
-{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
-
-const connection = mongoose.connection;
-
-connection.once("open", ()=>{
-    console.log("Database is Running");
-})
